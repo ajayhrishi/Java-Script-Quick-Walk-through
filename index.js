a60ba5c7f96988d345fa7cae2dea6cfc910d423b
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-03-18 08:16:44 +0530

    Testing the use of some basic variable and outputs

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(event) { // to prevent the javascript code running before the HTML code. 
+
 console.log("If this line is visisble in the screen, then it mean my java scrip is working");
-console.log("It seems it's working now. Yaaaay!");
+console.log("It seems it's working now. Yaaaay!");  // to give some outputs in the console which is not directly visible in the main page. 
 
 
-window.alert("this is the check if the alert function is working");
+window.alert("this is the check if the alert function is working"); // to give the alert box while opening the page. 
 
+let firstname = "Ajay";
+let age = 21;
+let isAdult = (age>18);
+
+console.log("Hey there, Iam ", firstname);
+console.log("Also, iam ", age," year old.");
+console.log("Do you think I am adult? Well it's ",isAdult);
+
+document.getElementById("A1").innerHTML ="Hey there, Iam " + firstname;
+document.getElementById("A2").innerHTML ="Also, iam "+ age+ " year old."; // to give the out put in the main page through taking the details from console. 
 
 
 //this is a comment sections that should not reflect in the page or effect the code.//
+});
