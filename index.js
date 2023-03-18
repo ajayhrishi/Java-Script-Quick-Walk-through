fbceb66adea5b1d791e797a62e7bde22e5a5a9ea
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-03-18 07:23:41 +0530

    Testing the Java script code is working for me to continue

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("If this line is visisble in the screen, then it mean my java scrip is working");
+console.log("It seems it's working now. Yaaaay!");
+
+
+window.alert("this is the check if the alert function is working");
+
+
+
+//this is a comment sections that should not reflect in the page or effect the code.//
