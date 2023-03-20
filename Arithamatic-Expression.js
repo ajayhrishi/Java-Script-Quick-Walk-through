44d559c66fe1f83bd10f622f052cb06067b233a4
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-03-20 10:07:23 +0530

    Testing the data converstaions using java script

--- Arithamatic-Expression.js
+++ Arithamatic-Expression.js
@@ -1,7 +1,16 @@
 window.onload = function() {
 let age = window.prompt("hey dude, please enter your age in the below box for me to test my code."); 
 console.log("So you are ", age, ", nice. Now let's see how we can display it in the main page.")
-
 document.getElementById("A1").innerHTML = "Here I am just testing if the age is updated as "+age+ " or not.";
+
+
+let ALA = Number("2.24");
+console.log(ALA, typeof ALA);
+
+let ALB = String(2.24);
+console.log(ALB, typeof ALB);
+
+let ALC = Boolean(2.24);
+console.log(ALC, typeof ALC);
 
 }
