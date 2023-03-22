4ca38020d0d98009b722a35911aec4468a9b6931
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-03-22 07:45:18 +0530

    Tested the const variable

--- To-count-the-surface-area-of-circle/Circle.js
+++ To-count-the-surface-area-of-circle/Circle.js
@@ -1,12 +1,16 @@
 window.onload = function() {
-let Pi = 3.14159;
+const PI = 3.14159;  // const used to deny the permission to change the variable value. 
+// it will give a little bit of data security and ensure the further codes are not accidently changing this
+// .. spesific variable's value. 
 let radius ; 
 let surface_area;
 
 radius =window.prompt("Enter the radius of the circle: ");
 radius= Number(radius);
+//PI = 2.355
 
-surface_area = radius*2*Pi;
+//PI=2.543
+surface_area = radius*2*PI;
 console.log("The surface area of the circle is: ", surface_area);
 
 }
