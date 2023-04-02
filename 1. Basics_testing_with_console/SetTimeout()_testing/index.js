e20baafa278aa9952f86b073574dbc33ea1c5099
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-04-02 08:35:00 +0530

    Testing the clearTimeout functions

--- 1. Basics_testing_with_console/SetTimeout()_testing/index.js
+++ 1. Basics_testing_with_console/SetTimeout()_testing/index.js
@@ -1,11 +1,4 @@
 
-
-setTimeout(function1, 10000);
-setTimeout(function2,15000);
-setTimeout(function3,5000 )
-
-
-
 function function1(){
 
     document.getElementById("function1").innerHTML = "This is the first function but supposed to only excute after 10 seconds";
@@ -20,5 +13,22 @@ function function2(){
 function function3(){
 
     document.getElementById("function3").innerHTML = "This is the 3rd function but supposed to only excute after 5 seconds";
-    
 }
+
+let a = setTimeout(function1, 10000);
+let b = setTimeout(function2,15000);
+let c = setTimeout(function3,5000 )
+
+
+document.getElementById("Skip").onclick = function()
+{
+   clearTimeout(a);
+   clearTimeout(b);
+   clearTimeout(c);
+   function1();
+   function2();
+   function3();
+
+}
+
+
