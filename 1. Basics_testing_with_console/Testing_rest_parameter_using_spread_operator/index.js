059209726b14ac31b7d4248e8ee3e925f72d4558
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-04-13 00:30:04 +0530

    Tested the rest parameter methoad

--- 1. Basics_testing_with_console/Testing_rest_parameter_using_spread_operator/index.js
+++ 1. Basics_testing_with_console/Testing_rest_parameter_using_spread_operator/index.js
@@ -2,15 +2,15 @@ import * as o from "../../OutPut.mjs"
 
 o.Create_MH("heading",`This is page is to test the rest parameter functions`);
 o.Create_A_R("as", `To view the source code, please refer to the JS sheet`);
-let numbers =1
+let numbers =0;
 o.Create_A("ad", `Please enter the amounts that you would like to pass in to the array`);
-
-
 
 
 o.Create_inputbox(numbers,"Number"); 
 o.br();
-
+let quick;
+let userarray;
+let inID;
 o.Create_buttonB("+","+");
 o.Create_buttonB("-","Del");
 o.Create_button("id", "Submit");
@@ -21,11 +21,24 @@ d.onclick = function(){removeInput();}
 button.onclick = function() {extrainput(); }
 
 
+c.onclick = function(){
+    
+    for (let e=1; e<=numbers; e++)
+    {  
+       inID = "I"+e;
+       quick = o.quickaccess(inID);
+       userarray[e] = quick.value;
+    }
+
+    o.Create_A(`o`,"hellow there");
+}
+
 /*took sometime, but figured out the alogorithm to make extra inputs and move the submit button along with it"*/
 function extrainput(){  
     o.br();
     numbers +=1;
-    o.Create_inputbox(numbers,"Number"); 
+    inID = "I"+numbers;
+    o.Create_inputbox(inID,"Number"); 
     o.br();
     button.remove()
     c.remove();
---
+++ 2.Quick_go_through/reset_parameter/index.js
@@ -0,0 +1,15 @@
+
+
+function added(x,y,...numbers)
+{
+  let total = 0;
+  for(number of numbers)
+  {
+    total += number;
+  }
+  return total;
+
+}
+
+
+console.log(added(2,6,23,234,54,213,45,2312,123));
